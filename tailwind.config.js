d8079f2b9c190fdc295b280c842ddc5589dbb460
Yashraj Dawkhar <dawkharyashraj@gmail.com>
2024-12-11 17:12:23 +0530

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily:{
-      'gullia': ['Gullia'],
-      'libre-franklin': ['Libre'],
+      'gullia': ['"Gullia"'],
+      'libre-franklin': ['"Libre"'],
     }
   },
   plugins: [],
